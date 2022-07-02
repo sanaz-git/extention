84ce9ab6f1f424459479beb4690c59f6e512dd05
Sanaz <szeraati69@gmail.com>
2022-07-02 17:48:51 +0430

    create extention

---
+++ background.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('onInstalled...');
+
+  // create alarm after extension is installed / upgraded
+  chrome.alarms.create('startRequest', { periodInMinutes: 4 });
+  startRequest();
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  startRequest();
+});
+
+async function startRequest() {
+  const response = await fetch('https://api.quotable.io/random');
+  const newData = await response.json();
+  const data = `${newData.content} —${newData.author}`;
+  console.log(data);
+
+  var options = {
+    title: 'Random Quotes',
+    message: data,
+    iconUrl: '/images/favicon-16x16.png',
+    type: 'basic',
+    // requireInteraction: true
+  };
+  chrome.notifications.create('', options);
+}
